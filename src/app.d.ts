97dfb3f44003123a179683e9b2955c3ac5434e0e
Jon Saw <jon.saw@gmail.com>
2022-09-16 12:20:18 +0800

    renamed hooks and set language locals

--- src/app.d.ts
+++ src/app.d.ts
@@ -2,7 +2,9 @@
 // for information about these interfaces
 // and what to do when importing types
 declare namespace App {
-	// interface Locals {}
+	interface Locals {
+		language?: string;
+	}
 	// interface PageData {}
 	// interface PageError {}
 	// interface Platform {}
--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,12 +1,15 @@
+import { locale } from '$lib/translations';
 import type { Handle } from '@sveltejs/kit';
 import * as cookie from 'cookie';
-import { locale } from './lib/translations';
 
 export const handle: Handle = async ({ event, resolve }) => {
 	const cookies = cookie.parse(event.request.headers.get('cookie') || '');
 
 	if (cookies.language) {
-		locale.set(cookies.language || 'en');
+		locale.set(cookies.language);
+
+		// Attach user setting into local env (this is optional)
+		event.locals.language = cookies.language;
 	}
 
 	const response = await resolve(event);
